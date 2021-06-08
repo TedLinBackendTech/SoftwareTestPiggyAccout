5d1e0f9cd8cda9590bb7e0decef428c21cf489ff
TedLin <t109598073@ntut.org.tw>
2021-06-08 15:49:40 +0800

    [feature] add buttons in AdvanceFunctionsPage.java in testUtils

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 1. 先啟動appium server
 2. 啟動android avd manager ,啟動裝置
 3. 執行測試 
+
+PS 執行UT前需確認，沒有額外的appium session開啟
--- src/test/java/accountfeature/AddNewAccountTest.java
+++ src/test/java/accountfeature/AddNewAccountTest.java
@@ -1,6 +1,16 @@
 package accountfeature;
 
-public class AddNewAccountTest {
+import org.testng.annotations.BeforeMethod;
+import testUtil.AbstractTest;
 
+import java.net.MalformedURLException;
+
+public class AddNewAccountTest extends AbstractTest {
+    @BeforeMethod
+    @Override
+    public void setUp() throws MalformedURLException {
+        super.setUp();
+        buttonNavigationBar.getAdvanceFunctionsButton().click();
+    }
 
 }
