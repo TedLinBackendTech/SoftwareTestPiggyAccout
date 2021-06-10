09a2833f49b1782a1d56e44f5e315dc13d0ef5d6
喻慶雲 <peteryu54089@gmail.com>
2021-06-10 22:09:53 +0800

    remove unnecessary import

--- src/test/java/reportfeature/CheckAccountListTest.java
+++ src/test/java/reportfeature/CheckAccountListTest.java
@@ -1,6 +1,5 @@
 package reportfeature;
 
-import io.appium.java_client.MobileElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
