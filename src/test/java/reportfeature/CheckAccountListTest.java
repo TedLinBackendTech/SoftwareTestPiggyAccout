8b747022af71e9cd41fcc2cd632dfeebffa7ce2e
喻慶雲 <peteryu54089@gmail.com>
2021-06-10 19:23:31 +0800

    finish check account list

--- src/test/java/reportfeature/CheckAccountListTest.java
+++ src/test/java/reportfeature/CheckAccountListTest.java
@@ -1,15 +1,10 @@
 package reportfeature;
 
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import testUtil.AbstractTest;
-import testUtil.AccountType;
-import testUtil.ButtonNavigationBar;
-import testUtil.Calculator;
 
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
@@ -50,15 +45,15 @@ public class CheckAccountListTest extends AbstractTest {
         buttonNavigationBar.getAccountListButton().click();
 
         String xpath = "//android.widget.TextView[@content-desc=\"新增\"]";
-        MobileElement addButton = (MobileElement)driver.findElementByXPath(xpath);
+        MobileElement addButton = driver.findElementByXPath(xpath);
         addButton.click();
 
         xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.EditText[1]";
-        MobileElement nameField = (MobileElement)driver.findElementByXPath(xpath);
+        MobileElement nameField = driver.findElementByXPath(xpath);
         nameField.sendKeys("新光銀行");
 
         xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.TextView[5]";
-        MobileElement amountField = (MobileElement)driver.findElementByXPath(xpath);
+        MobileElement amountField = driver.findElementByXPath(xpath);
         amountField.click();
         for (char ch: "1200".toCharArray()) {
             calculator.getButton(ch).click();
@@ -66,7 +61,7 @@ public class CheckAccountListTest extends AbstractTest {
         calculator.getOk().click();
 
         xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.Button";
-        MobileElement saveButton = (MobileElement)driver.findElementByXPath(xpath);
+        MobileElement saveButton = driver.findElementByXPath(xpath);
         saveButton.click();
 
         boolean isValid = true;
@@ -84,6 +79,71 @@ public class CheckAccountListTest extends AbstractTest {
         }
         xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[2]/android.widget.TextView[3]";
         if (!driver.findElementByXPath(xpath).getText().equals("$1,200")) {
+            isValid = false;
+        }
+        assertTrue(isValid);
+
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[2]/android.widget.ImageView";
+        MobileElement arrowButton = driver.findElementByXPath(xpath);
+        arrowButton.click();
+
+        xpath = "//android.widget.TextView[@content-desc=\"編輯\"]";
+        MobileElement editButton = driver.findElementByXPath(xpath);
+        editButton.click();
+
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.TextView[5]";
+        amountField = driver.findElementByXPath(xpath);
+        amountField.click();
+        for (char ch: "1300".toCharArray()) {
+            calculator.getButton(ch).click();
+        }
+        calculator.getOk().click();
+
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.Button";
+        saveButton = driver.findElementByXPath(xpath);
+        saveButton.click();
+        buttonNavigationBar.move(104,218,119,223);
+
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView[2]";
+        if (!driver.findElementByXPath(xpath).getText().equals("$1,300")) {
+            isValid = false;
+        }
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[2]";
+        if (!driver.findElementByXPath(xpath).getText().equals("$1,300")) {
+            isValid = false;
+        }
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[2]/android.widget.TextView[1]";
+        if (!driver.findElementByXPath(xpath).getText().equals("新光銀行")) {
+            isValid = false;
+        }
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[2]/android.widget.TextView[3]";
+        if (!driver.findElementByXPath(xpath).getText().equals("$1,300")) {
+            isValid = false;
+        }
+        assertTrue(isValid);
+
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[2]/android.widget.ImageView";
+        arrowButton = driver.findElementByXPath(xpath);
+        arrowButton.click();
+
+        xpath = "//android.widget.TextView[@content-desc=\"編輯\"]";
+        editButton = driver.findElementByXPath(xpath);
+        editButton.click();
+
+        xpath = "//android.widget.TextView[@content-desc=\"刪除\"]";
+        MobileElement deleteButton = driver.findElementByXPath(xpath);
+        deleteButton.click();
+
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.Button[2]";
+        MobileElement confirmButton = driver.findElementByXPath(xpath);
+        confirmButton.click();
+
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView[2]";
+        if (!driver.findElementByXPath(xpath).getText().equals("$0")) {
+            isValid = false;
+        }
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[2]";
+        if (!driver.findElementByXPath(xpath).getText().equals("$0")) {
             isValid = false;
         }
         assertTrue(isValid);
