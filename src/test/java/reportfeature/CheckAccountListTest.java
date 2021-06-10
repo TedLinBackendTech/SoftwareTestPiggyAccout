7d57a2a055ba0a8282d5f8290c3a66ff18cfddf3
喻慶雲 <peteryu54089@gmail.com>
2021-06-10 18:07:04 +0800

    add check account list

--- src/test/java/reportfeature/CheckAccountListTest.java
+++ src/test/java/reportfeature/CheckAccountListTest.java
@@ -36,12 +36,56 @@ public class CheckAccountListTest extends AbstractTest {
 
     @Test
     public void Test_check_account_list_empty() {
-        boolean isAccountListEmpty = false;
-        String xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView[2]";
         buttonNavigationBar.getAccountListButton().click();
-        if (driver.findElementByXPath(xpath).getText().equals("$0")) {
-            isAccountListEmpty = true;
+        boolean isValid = true;
+        String xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView[2]";
+        if (!driver.findElementByXPath(xpath).getText().equals("$0")) {
+            isValid = false;
         }
-        assertTrue(isAccountListEmpty);
+        assertTrue(isValid);
+    }
+
+    @Test
+    public void Test_check_account_list() {
+        buttonNavigationBar.getAccountListButton().click();
+
+        String xpath = "//android.widget.TextView[@content-desc=\"新增\"]";
+        MobileElement addButton = (MobileElement)driver.findElementByXPath(xpath);
+        addButton.click();
+
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.EditText[1]";
+        MobileElement nameField = (MobileElement)driver.findElementByXPath(xpath);
+        nameField.sendKeys("新光銀行");
+
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.TextView[5]";
+        MobileElement amountField = (MobileElement)driver.findElementByXPath(xpath);
+        amountField.click();
+        for (char ch: "1200".toCharArray()) {
+            calculator.getButton(ch).click();
+        }
+        calculator.getOk().click();
+
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.Button";
+        MobileElement saveButton = (MobileElement)driver.findElementByXPath(xpath);
+        saveButton.click();
+
+        boolean isValid = true;
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView[2]";
+        if (!driver.findElementByXPath(xpath).getText().equals("$1,200")) {
+            isValid = false;
+        }
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[2]";
+        if (!driver.findElementByXPath(xpath).getText().equals("$1,200")) {
+            isValid = false;
+        }
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[2]/android.widget.TextView[1]";
+        if (!driver.findElementByXPath(xpath).getText().equals("新光銀行")) {
+            isValid = false;
+        }
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[2]/android.widget.TextView[3]";
+        if (!driver.findElementByXPath(xpath).getText().equals("$1,200")) {
+            isValid = false;
+        }
+        assertTrue(isValid);
     }
 }
