3f9d780a6d6a258472c02dee45fe07b44aed0c1d
wall1350 <wall1350@gmail.com>
2021-06-12 21:11:41 +0800

    修改添加收支紀錄，目前已經完工

--- src/test/java/incomefeature/AddExpenseTest.java
+++ src/test/java/incomefeature/AddExpenseTest.java
@@ -105,7 +105,92 @@ public class AddExpenseTest extends AbstractTest {
         el12.click();
     }
 
-    public void delete_temp_data(){
+    private void  create_a_account(String accountName){
+        String amount = "1200";
+        MobileElement el2 = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/iv_profile");
+        el2.click();
+        MobileElement el3 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[2]/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.ImageView");
+        el3.click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        MobileElement el4 = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/btn_create");
+        el4.click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        //InputData
+        MobileElement el5 = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/et_name");
+        el5.sendKeys(accountName);
+        MobileElement inputQuotaField = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/tv_current_amount");
+        inputQuotaField.click();
+        for (char ch: amount.toCharArray()) {
+            calculator.getButton(ch).click();
+        }
+        calculator.getOk().click();
+        MobileElement saveButton = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/btn_save");
+        saveButton.click();
+    }
+
+    private void delete_a_account(String accountName){
+        buttonNavigationBar.getAdvanceFunctionsButton().click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        advanceFunctionsPage.getAccountOrderButton().click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        int index = 0;
+        String xpath = "";
+        int acoountListLength = driver.findElementsByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup").size();
+        for(index = 1; index <= acoountListLength; index++){
+            xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[" + index + "]/android.widget.TextView";
+            if(driver.findElementByXPath(xpath).getText().equals(accountName)){
+                break;
+            }
+        }
+
+        driver.findElementByXPath(xpath).click();
+        MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("刪除");
+        el1.click();
+        MobileElement el12 = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/btn_confirm");
+        el12.click();
+    }
+
+    private void create_a_regulation(String periodName){
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        buttonNavigationBar.getAdvanceFunctionsButton().click();
+        MobileElement el1 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[3]/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.ImageView");
+        el1.click();
+        MobileElement el2 = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/btn_create");
+        el2.click();
+        MobileElement el3 = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/et_name");
+        el3.sendKeys(periodName);
+        MobileElement el4 = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/et_duration");
+        el4.sendKeys("1");
+        MobileElement el5 = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/btn_save");
+        el5.click();
+
+    }
+
+    private void delete_a_regulation(String periodName){
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        buttonNavigationBar.getAdvanceFunctionsButton().click();
+        MobileElement el1 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[3]/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.ImageView");
+        el1.click();
+        int index = 0;
+        String xpath = "";
+        int acoountListLength = driver.findElementsByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup").size();
+        for(index = 1; index <= acoountListLength; index++){
+            xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[" + index + "]/android.widget.TextView";
+            if(driver.findElementByXPath(xpath).getText().equals(periodName)){
+                break;
+            }
+        }
+
+        driver.findElementByXPath(xpath).click();
+        MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("刪除");
+        el2.click();
+        MobileElement el3 = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/btn_confirm");
+        el3.click();
+
+    }
+
+    private void delete_temp_data(){
         MobileElement el19 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[2]");
         el19.click();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
@@ -207,22 +292,29 @@ public class AddExpenseTest extends AbstractTest {
 
     @Test
     public void Test_add_expense_tc3(){
-        //創立使用者自訂類別
-        create_a_category("收入類別新增測試");
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        driver.navigate().back();
-        // todo:使用者創建帳戶
-
-        // todo:使用者創建週期
-        MobileElement recordListButton = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/iv_record_list");
-        recordListButton.click();
 
         String date="28 June 2021";
         String money="-1000";
         String category="收入類別新增測試";
-        String accountName="其他";
-        String regulation= "每天";
+        String accountName="測試銀行帳號";
+        String regulation= "測試週期";
         String comment="收入類別新增測試我要大於五十字我要大於五十字我要大於五十字我要大於五十字我要大於五十字我要大於五十字我要大於五十字";
+
+        //創立使用者自訂類別
+        create_a_category(category);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.navigate().back();
+
+        // todo:使用者創建帳戶
+        create_a_account(accountName);
+        driver.navigate().back();
+
+        // todo:使用者創建週期
+        create_a_regulation(regulation);
+        driver.navigate().back();
+
+        MobileElement recordListButton = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/iv_record_list");
+        recordListButton.click();
 
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         MobileElement el1 = (MobileElement) driver.findElementById("com.coceany.piggyaccounting:id/iv_create_record");
@@ -297,7 +389,17 @@ public class AddExpenseTest extends AbstractTest {
         this.delete_temp_data();
         //刪除測試類別
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        delete_category("收入類別新增測試");
+        delete_category(category);
+        driver.navigate().back();
+        //刪除測試帳戶
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        delete_a_account(accountName);
+        driver.navigate().back();
+
+        //刪除測試週期
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        delete_a_regulation(regulation);
+        driver.navigate().back();
     }
 
     @Test
